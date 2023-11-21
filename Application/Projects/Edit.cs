9adccd4818f364d2546d27b2c1831319c3f1d5e2
Ciaran <ciarank500@gmail.com>
2023-11-21 16:00:57 +0000

    added api CRUD functionality

---
+++ Application/Projects/Edit.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Projects
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Project Project { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+            _mapper = mapper;
+            _context = context;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var project = await _context.Projects.FindAsync(request.Project.Id);
+
+                _mapper.Map(request.Project, project);
+
+                await _context.SaveChangesAsync();
+
+
+            }
+        }
+    }
+}
